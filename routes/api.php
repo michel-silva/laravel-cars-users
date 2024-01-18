05e54037ab875f269096b251a821c15fa55f2399
Michel Silva <silva.michel.b@gmail.com>
2024-01-17 23:08:54 -0300

    create routes

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,8 @@
 <?php
 
+use App\Http\Controllers\BrandController;
+use App\Http\Controllers\CarController;
+use App\Http\Controllers\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -17,3 +20,7 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::get('brand', [BrandController::class, 'index']);
+Route::apiResource('user', UserController::class);
+Route::apiResource('car', CarController::class);
