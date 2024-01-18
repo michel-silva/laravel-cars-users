1fc5ca441b1b05db0efc6abf35b06e9e3d20794c
Michel Silva <silva.michel.b@gmail.com>
2024-01-18 16:09:34 -0300

    create static property

--- database/seeders/BrandSeeder.php
+++ database/seeders/BrandSeeder.php
@@ -8,93 +8,93 @@ use Illuminate\Database\Seeder;
 
 class BrandSeeder extends Seeder
 {
+    static $brand_list = [
+        'Chevrolet' => [
+            ['name' => 'Cruze'],
+            ['name' => 'Onix'],
+            ['name' => 'Trax'],
+            ['name' => 'Equinox'],
+        ],
+        'Volkswagen' => [
+            ['name' => 'Golf'],
+            ['name' => 'Jetta'],
+            ['name' => 'Tiguan'],
+            ['name' => 'Atlas'],
+        ],
+        'Ford' => [
+            ['name' => 'Fiesta'],
+            ['name' => 'Focus'],
+            ['name' => 'Mustang'],
+            ['name' => 'Explorer'],
+        ],
+        'Fiat' => [
+            ['name' => '500'],
+            ['name' => 'Punto'],
+            ['name' => 'Toro'],
+            ['name' => 'Doblo'],
+        ],
+        'Renault' => [
+            ['name' => 'Clio'],
+            ['name' => 'Megane'],
+            ['name' => 'Captur'],
+            ['name' => 'Koleos'],
+        ],
+        'Toyota' => [
+            ['name' => 'Corolla'],
+            ['name' => 'Camry'],
+            ['name' => 'RAV4'],
+            ['name' => 'Highlander'],
+        ],
+        'Honda' => [
+            ['name' => 'Civic'],
+            ['name' => 'Accord'],
+            ['name' => 'CR-V'],
+            ['name' => 'Pilot'],
+        ],
+        'Hyundai' => [
+            ['name' => 'Elantra'],
+            ['name' => 'Sonata'],
+            ['name' => 'Santa Fe'],
+            ['name' => 'Palisade'],
+        ],
+        'Nissan' => [
+            ['name' => 'Altima'],
+            ['name' => 'Maxima'],
+            ['name' => 'Rogue'],
+            ['name' => 'Pathfinder'],
+        ],
+        'Jeep' => [
+            ['name' => 'Cherokee'],
+            ['name' => 'Grand Cherokee'],
+            ['name' => 'Wrangler'],
+            ['name' => 'Gladiator'],
+        ],
+        'Peugeot' => [
+            ['name' => '208'],
+            ['name' => '308'],
+            ['name' => '3008'],
+            ['name' => '5008'],
+        ],
+        'Citroën' => [
+            ['name' => 'C3'],
+            ['name' => 'C4'],
+            ['name' => 'C5'],
+            ['name' => 'C4 Cactus'],
+        ],
+        'Mitsubishi' => [
+            ['name' => 'Outlander'],
+            ['name' => 'Pajero'],
+            ['name' => 'L200'],
+            ['name' => 'Eclipse Cross'],
+        ],
+    ];
+
     /**
      * Run the database seeds.
      */
     public function run(): void
     {
-        $brand_list = [
-            'Chevrolet' => [
-                ['name' => 'Cruze'],
-                ['name' => 'Onix'],
-                ['name' => 'Trax'],
-                ['name' => 'Equinox'],
-            ],
-            'Volkswagen' => [
-                ['name' => 'Golf'],
-                ['name' => 'Jetta'],
-                ['name' => 'Tiguan'],
-                ['name' => 'Atlas'],
-            ],
-            'Ford' => [
-                ['name' => 'Fiesta'],
-                ['name' => 'Focus'],
-                ['name' => 'Mustang'],
-                ['name' => 'Explorer'],
-            ],
-            'Fiat' => [
-                ['name' => '500'],
-                ['name' => 'Punto'],
-                ['name' => 'Toro'],
-                ['name' => 'Doblo'],
-            ],
-            'Renault' => [
-                ['name' => 'Clio'],
-                ['name' => 'Megane'],
-                ['name' => 'Captur'],
-                ['name' => 'Koleos'],
-            ],
-            'Toyota' => [
-                ['name' => 'Corolla'],
-                ['name' => 'Camry'],
-                ['name' => 'RAV4'],
-                ['name' => 'Highlander'],
-            ],
-            'Honda' => [
-                ['name' => 'Civic'],
-                ['name' => 'Accord'],
-                ['name' => 'CR-V'],
-                ['name' => 'Pilot'],
-            ],
-            'Hyundai' => [
-                ['name' => 'Elantra'],
-                ['name' => 'Sonata'],
-                ['name' => 'Santa Fe'],
-                ['name' => 'Palisade'],
-            ],
-            'Nissan' => [
-                ['name' => 'Altima'],
-                ['name' => 'Maxima'],
-                ['name' => 'Rogue'],
-                ['name' => 'Pathfinder'],
-            ],
-            'Jeep' => [
-                ['name' => 'Cherokee'],
-                ['name' => 'Grand Cherokee'],
-                ['name' => 'Wrangler'],
-                ['name' => 'Gladiator'],
-            ],
-            'Peugeot' => [
-                ['name' => '208'],
-                ['name' => '308'],
-                ['name' => '3008'],
-                ['name' => '5008'],
-            ],
-            'Citroën' => [
-                ['name' => 'C3'],
-                ['name' => 'C4'],
-                ['name' => 'C5'],
-                ['name' => 'C4 Cactus'],
-            ],
-            'Mitsubishi' => [
-                ['name' => 'Outlander'],
-                ['name' => 'Pajero'],
-                ['name' => 'L200'],
-                ['name' => 'Eclipse Cross'],
-            ],
-        ];
-
-        foreach($brand_list as $brand => $versions) {
+        foreach(self::$brand_list as $brand => $versions) {
             $brand = Brand::firstOrCreate(['name' => $brand]);
             foreach($versions as $version) {
                 $brand->versions()->firstOrCreate($version, []);
