b596fddea4fb247325e1585c3fed6cbe0f72355a
Michel Silva <silva.michel.b@gmail.com>
2024-01-17 23:20:47 -0300

    add soft deletes

--- app/Models/Version.php
+++ app/Models/Version.php
@@ -4,10 +4,11 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Version extends Model
 {
-    use HasFactory;
+    use HasFactory, SoftDeletes;
 
     protected $fillable = [
         'car_brand_id',
