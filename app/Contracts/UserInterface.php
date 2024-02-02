befaa8f35b8a57902f144dca64de5af1c7aab6eb
Michel Silva <silva.michel.b@gmail.com>
2024-02-02 10:10:18 -0300

    use crud interface

--- app/Contracts/UserInterface.php
+++ app/Contracts/UserInterface.php
@@ -6,12 +6,7 @@ use App\Classes\QueryParam;
 use App\Models\User;
 use Illuminate\Contracts\Pagination\LengthAwarePaginator;
 
-interface UserInterface {
-    public function getPaginate(QueryParam $query_param) : LengthAwarePaginator;
-    public function getById(int $user_id) : User;
-    public function create(array $user_data) : User;
-    public function update(int $user_id, array $user_data) : User;
-    public function delete(int $user_id) : User;
+interface UserInterface extends CrudInterface {
     public function assignCar(int $user_id, int $car_id) : User;
     public function unassignCar(int $user_id, int $car_id) : User;
     public function carsByUser(int $user_id, QueryParam $query_param) : LengthAwarePaginator;
