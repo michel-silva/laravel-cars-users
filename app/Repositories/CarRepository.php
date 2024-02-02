033d6945189c9550b770b3ccb6aaf93b21554c3d
Michel Silva <silva.michel.b@gmail.com>
2024-02-02 10:11:06 -0300

    use crud repository

--- app/Repositories/CarRepository.php
+++ app/Repositories/CarRepository.php
@@ -7,33 +7,10 @@ use App\Contracts\CarInterface;
 use App\Models\Car;
 use Illuminate\Contracts\Pagination\LengthAwarePaginator;
 
-class CarRepository implements CarInterface{
-    public function getPaginate(QueryParam $query_param) : LengthAwarePaginator 
-    {
-        return Car::paginate($query_param->per_page);
-    }
+class CarRepository extends CrudRepository implements CarInterface{
 
-    public function getById(int $car_id) : Car 
+    public function __construct() 
     {
-        return Car::findOrFail($car_id);
-    }
-
-    public function create(array $car_data) : Car 
-    {
-        return Car::create($car_data);
-    }
-
-    public function update(int $car_id, array $car_data) : Car 
-    {
-        $car = Car::findOrFail($car_id);
-        $car->update($car_data);
-        return $car;
-    }
-
-    public function delete(int $car_id) : Car 
-    {
-        $car = Car::findOrFail($car_id);
-        $car->delete();
-        return $car;
+        $this->model = new Car;
     }
 }
--- app/Repositories/UserRepository.php
+++ app/Repositories/UserRepository.php
@@ -8,41 +8,16 @@ use App\Models\Car;
 use App\Models\User;
 use Illuminate\Contracts\Pagination\LengthAwarePaginator;
 
-class UserRepository implements UserInterface {
-    public function getPaginate(QueryParam $query_param) : LengthAwarePaginator
+class UserRepository extends CrudRepository implements UserInterface {
+    
+    public function __construct() 
     {
-        return User::paginate($query_param->per_page);
-    }
-
-    public function getById(int $user_id) : User 
-    {
-        return User::findOrFail($user_id);
-    }
-
-    public function create(array $user_data) : User 
-    {
-        return User::create($user_data);
-    }
-
-    public function update(int $user_id, array $user_data) : User
-    {
-        $user = User::findOrFail($user_id);
-
-        unset($user_data['name']);
-        $user->update($user_data);
-        return $user;
-    }
-
-    public function delete(int $user_id) : User
-    {
-        $user = User::findOrFail($user_id);
-        $user->delete();
-        return $user;
+        $this->model = new User;
     }
 
     public function assignCar(int $user_id, int $car_id) : User
     {
-        $user = User::findOrFail($user_id);
+        $user = $this->model->findOrFail($user_id);
         $user->cars()->syncWithoutDetaching([$car_id]);
 
         return $user;
@@ -50,7 +25,7 @@ class UserRepository implements UserInterface {
 
     public function unassignCar(int $user_id, int $car_id) : User
     {
-        $user = User::findOrFail($user_id);
+        $user = $this->model->findOrFail($user_id);
         $user->cars()->detach($car_id);
 
         return $user;
